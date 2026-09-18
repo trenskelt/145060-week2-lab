cb2c93ca5ae74bb432bde89e6442c64f731bfd0a
Trent Skelton <sakurajisakuraji932@gmail.com>
2026-09-18 09:49:11 -0400

    Lab C: Adventure

---
+++ lab_c_adventure.py
@@ -0,0 +1,8 @@
+Bag_Items = ("Sword", "Shield", "Potion", "Map",)
+
+print("Welcome Adventurer! What is your name?")
+name = input()
+print("Hello, " + name + "! You have a " + Bag_Items[0] + " in your bag.")
+print("How many gold coins do you have?")
+gold_coins = int(input())
+print("You have " + gold_coins + " gold coins to spend.")
