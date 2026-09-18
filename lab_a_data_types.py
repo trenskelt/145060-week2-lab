78002545c4192da573201463d52a02a494ddd39b
Trent Skelton <sakurajisakuraji932@gmail.com>
2026-09-18 08:40:59 -0400

    Initial commit: add README

---
+++ lab_a_data_types.py
@@ -0,0 +1,25 @@
+"""
+This file will do something at some point in the future.
+Author: Trent Skelton
+9/18/2026
+"""
+#Topic of choice: Video Games
+
+fortnite = "Battle Royale Shooter" # String
+health = (100) # Integer
+fortnite_is_good = False # Boolean
+fortnite_weaponry = ["Scar", "Pump", "Heals"] # List
+fortnite_version = 41.3 # Float
+fortnite_skins = ("Jonesy", "Peely", "Star Bomber") # Tuple
+fortnite_builds = {"Wall", "Cone", "Floor", "Ramp"} # Set
+fortnite_savetheworld = {"GraveDigger": "ModdedWeapon"} # Dictionary
+
+print(fortnite)
+print(health + 5)
+print(fortnite_is_good)
+print(fortnite_weaponry)
+print(fortnite_version - 1.5)
+print(fortnite_skins)
+print(fortnite_builds)
+print(fortnite_savetheworld)
+
